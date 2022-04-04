4c3aa974c6a1e08417488d2eb4a66905be0e386e
翟思丰 <1482451437@qq.com>
2022-04-04 20:39:34 +0800

    修改正确的远程地址

--- filetoc.config.js
+++ filetoc.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  remoteUrl: 'https://github.com/zsf1482451437/my_algorithm_history.git',   // your repo address
+  remoteUrl: 'https://github.com/zsf1482451437',   // your repo address
   mainBranch: 'main',  // your default branch. default 'main'
   dirPath: './src/',  //  the dir where you want to gengerate the toc. default '.'
   mdPath: './README.md',  // the markdown  file path.  default 'README.md'
